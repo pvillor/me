0bf3336f1dc02a97e809d9f6ec04e63ead97bdbf
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-10-30 15:36:02 -0400

    config: router and font family

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './router'
+
 export function App() {
-  return <h1 className="text-yellow-100">Link Start</h1>
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
