87df580c79c624b8b03a27c3ac29f0539070ef9a
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-10-30 15:36:59 -0400

    config: add projects page in router

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/default'
+import { Projects } from './pages/projects'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<h1>Link Start</h1>} />
+        <Route path="/projects" element={<Projects />} />
       </Route>
     </Routes>
   )
