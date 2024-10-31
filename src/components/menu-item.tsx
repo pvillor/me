d38cbe2a9a63f9ecf8f05bd1ebc39ecf01533306
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-10-31 16:03:57 -0400

    feat: navigation by menu

--- src/components/menu-item.tsx
+++ src/components/menu-item.tsx
@@ -1,14 +1,32 @@
 import type { Icon, IconProps } from '@phosphor-icons/react'
 import { Tooltip } from './tooltip'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export interface MenuItemProps extends IconProps {
   icon: Icon
   title: string
+  route: string
+  isExternalUrl?: boolean
 }
 
-export function MenuItem({ icon: Icon, title, ...props }: MenuItemProps) {
+export function MenuItem({
+  icon: Icon,
+  title,
+  route,
+  isExternalUrl,
+  ...props
+}: MenuItemProps) {
+  const navigate = useNavigate()
   const [isTooltipVisible, setIsTooltipVisible] = useState(false)
+
+  function handleNavigate(route: string) {
+    if (isExternalUrl) {
+      window.open(route)
+    } else {
+      navigate(route)
+    }
+  }
 
   return (
     <div className="flex flex-col items-center">
@@ -17,6 +35,7 @@ export function MenuItem({ icon: Icon, title, ...props }: MenuItemProps) {
         className="bg-zinc-800/35 p-3 rounded-full transition-transform duration-300 shadow-sm shadow-zinc-700 hover:scale-150 hover:mx-2 hover:mb-2"
         onMouseEnter={() => setIsTooltipVisible(true)}
         onMouseLeave={() => setIsTooltipVisible(false)}
+        onClick={() => handleNavigate(route)}
       >
         <Icon {...props} size={24} weight="fill" className="text-zinc-100" />
       </button>
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -5,9 +5,14 @@ export function Menu() {
   return (
     <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-8">
       <div className="flex items-end gap-3 p-2 rounded-full shadow-sm shadow-zinc-700">
-        <MenuItem icon={House} title="Home" />
-        <MenuItem icon={Code} title="Projects" />
-        <MenuItem icon={GithubLogo} title="GitHub" />
+        <MenuItem icon={House} title="Home" route="/" />
+        <MenuItem icon={Code} title="Projects" route="/projects" />
+        <MenuItem
+          icon={GithubLogo}
+          title="GitHub"
+          route="https://github.com/pvillor"
+          isExternalUrl
+        />
       </div>
     </div>
   )
